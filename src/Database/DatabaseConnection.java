cc4188d1a8158f943fe318dc11adb9f7634e4e80
mjlpjacoavans <58549815+mjlpjacoavans@users.noreply.github.com>
2020-01-15 14:47:07 +0100

    Created the Database domain and the DatabaseConnection class

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
+public class DatabaseConnection {
+
+    private static Connection connection;
+    private static String databseName = "Netflix";
+    private static  String connectionUrl = String.format("jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=%s;integratedSecurity=true;",databseName);
+
+    public static Connection getConn(){
+        try{
+            return DriverManager.getConnection(connectionUrl);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
