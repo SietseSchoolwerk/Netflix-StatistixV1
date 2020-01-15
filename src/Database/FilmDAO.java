8de686993eb26ce569ad9330778ba8e289d5aa66
mjlpjacoavans <58549815+mjlpjacoavans@users.noreply.github.com>
2020-01-15 16:56:34 +0100

    Add a constructor to FilmDAO

--- src/Database/FilmDAO.java
+++ src/Database/FilmDAO.java
@@ -6,6 +6,7 @@ import Domain.Film;
 public class FilmDAO extends ProgramDAO{
 
     private static Connection connection = DatabaseConnection.getConn();
+    public void FilmDAO(Connection connection) { this.connection = connection; }
 
     public Film getFilm(int ProgramId){
         return (Film)super.getProgram(ProgramId);
