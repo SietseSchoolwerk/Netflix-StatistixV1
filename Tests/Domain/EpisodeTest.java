d845511d96ae460735e2f2855a6f6fcc3e6f7ec3
daanhayman <daanhayman@icloud.com>
2020-01-16 21:47:19 +0100

    Tests made for episodes
    Tests cases made for episodes.

---
+++ Tests/Domain/EpisodeTest.java
@@ -0,0 +1,16 @@
+package Domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpisodeTest {
+
+    @Test
+    void testSetFollowNumberValidWithStringShouldReturnTrue() {
+    }
+
+    @Test
+    void testSetFollowNumberInvalidWithStringShouldReturnFalse() {
+    }
+}
