c828ae9769254575a452c9501480ac23a065d590
daanhayman <daanhayman@icloud.com>
2020-01-16 14:02:07 +0100

    Updated film constructor to support age
    Updated film to support age

--- src/Domain/Film.java
+++ src/Domain/Film.java
@@ -3,8 +3,8 @@ package Domain;
 public class Film extends Program {
     private int programId;
 
-    public Film(int programId, String title, String genre, String language, String lengthOfTime) {
-        super(programId, title, genre, language, lengthOfTime);
+    public Film(int programId, String title, String genre, String language, String lengthOfTime, int age) {
+        super(programId, title, genre, language,age, lengthOfTime);
         this.programId = programId;
     }
 
