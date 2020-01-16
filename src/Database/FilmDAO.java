13abe46f7ec05ca8138b972d66493286129f8404
Sietse van Tilburg <sja.vantilburg@student.avans.nl>
2020-01-16 15:08:28 +0100

    Added getallmovies
    you can no get all movies

--- src/Database/FilmDAO.java
+++ src/Database/FilmDAO.java
@@ -3,6 +3,7 @@ package Database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import Domain.Episode;
 import Domain.Film;
@@ -31,6 +32,24 @@ public class FilmDAO extends ProgramDAO {
         ProgramDAO dao = new ProgramDAO();
         Program prog = dao.getProgram(programId);
         return new Film(programId, prog.getTitle(), prog.getGenre(), prog.getLanguage(), prog.getLengthOfTime(), prog.getAge());
+    }
+
+    public ArrayList<Program> getAllMovies() {
+        String sql = "SELECT * FROM Programma WHERE NOT Programma.ProgramId \n" +
+                                " IN (SELECT Episode.ProgramId FROM Episode);";
+
+        try {
+            ArrayList<Program> result = new ArrayList<>();
+            PreparedStatement statement = this.connection.prepareStatement(sql);
+            ResultSet r = statement.executeQuery();
+            while(r.next()) {
+                result.add(new Film(r.getInt("ProgramId"),r.getString("Title"),r.getString("Genre"),r.getString("Language"), r.getString("playtime"), r.getInt("Age")));
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
