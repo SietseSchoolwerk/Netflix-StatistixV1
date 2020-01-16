f2548b8c8d3183f5fe4a0efea548f18aa2764569
daanhayman <daanhayman@icloud.com>
2020-01-16 21:24:55 +0100

    Added packages within test for their corresponding tests
    Look at title

---
+++ Tests/Domain/AccountTest.java
@@ -0,0 +1,68 @@
+package Domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void testSetValidEmailShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetInvalidEmailShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetValidPasswordShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetInvalidPasswordShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetValidSubscriberShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetInvalidSubscriberShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetValidAdressShouldReturnTrue() {
+        
+    }
+
+    @Test
+    void testSetInvalidAdressShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetValidCityShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testSetInvalidCityShouldReturnTrue() {
+
+    }
+
+    @Test
+    void testAccountConstructorWithInvalidDataShouldReturnFalse() {
+
+    }
+
+    @Test
+    void testAccountConstructorWithValidDataShouldReturnTrue() {
+
+    }
+}
