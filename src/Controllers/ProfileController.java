0db2454ecb0ef265e785af1beefc009aab8883ea
Sietse van Tilburg <sja.vantilburg@student.avans.nl>
2020-01-17 13:44:08 +0100

    Fixed wrong alert headers

--- src/Controllers/ProfileController.java
+++ src/Controllers/ProfileController.java
@@ -36,7 +36,7 @@ public class ProfileController implements EventHandler<ActionEvent> {
         this.stage = stage;
         this.alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Warning Dialog");
-        alert.setHeaderText("Error occured while creating account");
+        alert.setHeaderText("Error occured with profile");
     }
 
     public void handle(ActionEvent event) {
