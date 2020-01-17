bb94ee96db9b6665ebf36f43d7cd1570ba176e43
daanhayman <daanhayman@icloud.com>
2020-01-17 13:44:48 +0100

    Commenting of film

--- src/Domain/Film.java
+++ src/Domain/Film.java
@@ -1,8 +1,18 @@
 package Domain;
 
+//Film class for the netflix program. This is not used for editing only for displaying in lists.
 public class Film extends Program {
     private int programId;
 
+    /**
+     * Default constructor for film
+     * @param programId
+     * @param title
+     * @param genre
+     * @param language
+     * @param lengthOfTime
+     * @param age
+     */
     public Film(int programId, String title, String genre, String language, String lengthOfTime, int age) {
         super(programId, title, genre, language,age, lengthOfTime);
         this.programId = programId;
